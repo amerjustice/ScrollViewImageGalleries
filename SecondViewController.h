31230fc25ef9dd70e898c9b5dd989f7f280e2b71
amerjustice <amerjustice@outlook.com>
2016-05-16 20:04:22 -0700

    added part 2.5

---
+++ SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  ScrollViewImageGalleries
+//
+//  Created by Susan Justice on 2016-05-16.
+//  Copyright © 2016 Amer Justice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@property (nonatomic) UIImage *currentImage;
+
+@end
---
+++ SecondViewController.m
@@ -0,0 +1,40 @@
+//
+//  SecondViewController.m
+//  ScrollViewImageGalleries
+//
+//  Created by Susan Justice on 2016-05-16.
+//  Copyright © 2016 Amer Justice. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController () <UIScrollViewDelegate>
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.imageView.image = self.currentImage;
+    
+    self.scrollView.delegate = self;
+    self.scrollView.minimumZoomScale = 1.0;
+    self.scrollView.maximumZoomScale = 4.0;
+    self.scrollView.zoomScale = 1.0;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (nullable UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return self.imageView;
+}
+
+
+@end
